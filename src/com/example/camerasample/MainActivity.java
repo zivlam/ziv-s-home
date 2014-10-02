10f7c3c1d857daafb3a190ad8fe00ee2a32d841f
ZIV <zivlamcc@gmail.com>
2014-10-02 23:41:14 +0800

    test

--- src/com/example/camerasample/MainActivity.java
+++ src/com/example/camerasample/MainActivity.java
@@ -26,130 +26,69 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+
 public class MainActivity extends Activity {
 
-	//Camera object
-	Camera mCamera;
-	//Preview surface
-	SurfaceView surfaceView;
-	//Preview surface handle for callback
-	SurfaceHolder surfaceHolder;
-	//Camera button
-	Button btnCapture;
-	//Note if preview windows is on.
-	boolean previewing;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		btnCapture = (Button) findViewById(R.id.btn_capture);
-		btnCapture.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				if (previewing)
-					mCamera.takePicture(shutterCallback, rawPictureCallback,
-							jpegPictureCallback);				
-			}
-		});
-		
-		surfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
-		surfaceHolder = surfaceView.getHolder();
-		surfaceHolder.addCallback(new SurfaceViewCallback());
-		//surfaceHolder.addCallback(this);
-		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
+    private ImageView resultView;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	ShutterCallback shutterCallback = new ShutterCallback() {
-		@Override
-		public void onShutter() {
-		}
-	};	
-	
-	PictureCallback rawPictureCallback = new PictureCallback() {
-		@Override
-		public void onPictureTaken(byte[] arg0, Camera arg1) {
-
-		}
-	};
-	
-	PictureCallback jpegPictureCallback = new PictureCallback() {
-		@Override
-		public void onPictureTaken(byte[] arg0, Camera arg1) {
-
-			String fileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
-					.toString()
-					+ File.separator
-					+ "PicTest_" + System.currentTimeMillis() + ".jpg";
-			File file = new File(fileName);
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdir();
-			}
-			
-			try {
-				BufferedOutputStream bos = new BufferedOutputStream(
-						new FileOutputStream(file));
-				bos.write(arg0);
-				bos.flush();
-				bos.close();		
-				scanFileToPhotoAlbum(file.getAbsolutePath());
-				Toast.makeText(MainActivity.this, "[Test] Photo take and store in" + file.toString(),Toast.LENGTH_LONG).show();
-			} catch (Exception e) {
-				Toast.makeText(MainActivity.this, "Picture Failed" + e.toString(),
-						Toast.LENGTH_LONG).show();
-			}
-		};
-	};
-	
-	public void scanFileToPhotoAlbum(String path) {
-
-        MediaScannerConnection.scanFile(MainActivity.this,
-                new String[] { path }, null,
-                new MediaScannerConnection.OnScanCompletedListener() {
-
-                    public void onScanCompleted(String path, Uri uri) {
-                        Log.i("TAG", "Finished scanning " + path);
-                    }
-                });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultView = (ImageView) findViewById(R.id.result_image);
     }
-	private final class SurfaceViewCallback implements android.view.SurfaceHolder.Callback {   
-		public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) 
-		{
-			if (previewing) {
-				mCamera.stopPreview();
-				previewing = false;
-			}
-	
-			try {
-				mCamera.setPreviewDisplay(arg0);
-				mCamera.startPreview();
-				previewing = true;
-			} catch (Exception e) {}
-		}
-		public void surfaceCreated(SurfaceHolder holder) {
-				mCamera = Camera.open();
-				// get Camera parameters
-				Camera.Parameters params = mCamera.getParameters();
 
-				List<String> focusModes = params.getSupportedFocusModes();
-				if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
-				  // Autofocus mode is supported
-				}
-			}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-			public void surfaceDestroyed(SurfaceHolder holder) {
-				mCamera.stopPreview();
-				mCamera.release();
-				mCamera = null;
-				previewing = false;
-			}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_select) {
+            resultView.setImageDrawable(null);
+            Crop.pickImage(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
+            beginCrop(result.getData());
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            handleCrop(resultCode, result);
+        }
+    }
+
+    private void beginCrop(Uri source) {
+        Uri outputUri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        new Crop(source).output(outputUri).asSquare().start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            resultView.setImageURI(Crop.getOutput(result));
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
+
